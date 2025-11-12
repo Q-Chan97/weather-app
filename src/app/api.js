2124849f3f9d745f9ed1f456493523f09a4c610a
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2025-11-12 14:11:47 -0600

    Make variables for url, api key, and unit group

--- src/app/api.js
+++ src/app/api.js
@@ -1,6 +1,10 @@
-export async function getLocation() { // Get London info
+export const baseURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
+export const API_KEY = "96A6976GH6KMUCQN6Y7QF8YRH"; // API key
+let unitGroup = "us"; // Default unit group is US Imperial
+
+export async function getLocation(locationTerm) { // Get Ueda info
     try {
-        let response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London,UK?key=96A6976GH6KMUCQN6Y7QF8YRH`);
+        let response = await fetch(`${baseURL}Ueda?unitGroup=${unitGroup}&key=${API_KEY}`);
         let locationData = await response.json();
         console.log(locationData);
     } catch(err) {
