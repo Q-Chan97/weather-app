2231c4df600c66c4ce210e55f464909e9cc7ec89
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2025-11-13 09:09:19 -0600

    Create search location function

--- src/app/api.js
+++ src/app/api.js
@@ -1,13 +1,18 @@
 export const baseURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
 export const API_KEY = "96A6976GH6KMUCQN6Y7QF8YRH"; // API key
-let unitGroup = "us"; // Default unit group is US Imperial
+export let unitGroup = "us"; // Default unit group is US Imperial
 
-export async function getLocation(locationTerm) { // Get Ueda info
+export async function getLocation(locationTerm) {
     try {
-        let response = await fetch(`${baseURL}Ueda?unitGroup=${unitGroup}&key=${API_KEY}`);
+        let response = await fetch(`${baseURL}${locationTerm}?unitGroup=${unitGroup}&key=${API_KEY}`);
         let locationData = await response.json();
         console.log(locationData);
     } catch(err) {
         console.log(err);
     }
+}
+
+export async function searchLocation() {
+    const searchTerm = document.getElementById("search-bar").value;
+    getLocation(searchTerm);
 }
