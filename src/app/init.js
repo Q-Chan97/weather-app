dda1f6ab791e4f890f09374cd5cec1a14968e0b5
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2025-11-18 08:47:31 -0600

    Update init function w/o searchLocation

--- src/app/init.js
+++ src/app/init.js
@@ -2,8 +2,10 @@
 
 import { getLocation } from "./api";
 import { runSearch } from "./eventListeners";
+import { updatePage } from "./weatherUI";
 
 export function init() {
     getLocation();
     runSearch();
+    updatePage();
 }
