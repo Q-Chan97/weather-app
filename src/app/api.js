f3bc41a369360817f7c3df94c50e3d90dc42a2c0
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2025-11-20 11:57:14 -0600

    Update returns for getLocation

--- src/app/api.js
+++ src/app/api.js
@@ -1,17 +1,25 @@
 export const baseURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
 export const API_KEY = "96A6976GH6KMUCQN6Y7QF8YRH"; // API key
 export let unitGroup = "us"; // Default unit group is US Imperial
+export let iconSet = "icons2"; // Return icon set 2 names
 
 export async function getLocation(locationTerm) {
     try {
-        let response = await fetch(`${baseURL}${locationTerm}?unitGroup=${unitGroup}&key=${API_KEY}`);
+        let response = await fetch(`${baseURL}${locationTerm}?unitGroup=${unitGroup}&iconSet=${iconSet}&key=${API_KEY}`);
         let locationData = await response.json();
         console.log(locationData);
 
+        const location = locationData.address;
+        const currentTemp = Math.round(locationData.currentConditions.temp);
         const conditions = locationData.currentConditions.conditions;
-        console.log(`Should be the first ${conditions}`);
+        const icon = locationData.currentConditions.icon;
 
-        return conditions;
+        return {
+            location,
+            currentTemp,
+            conditions,
+            icon,
+        };
     } catch(err) {
         console.log(err);
     }
