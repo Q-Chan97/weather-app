13cddff653fee6defb9be293cd143f3cd200ef8a
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2025-11-20 12:48:40 -0600

    Create function to capitalize location

--- src/app/weatherUI.js
+++ src/app/weatherUI.js
@@ -19,9 +19,14 @@ function updateCurrentConditions() {
     const conditions = document.getElementById("conditions");
     const mainImg = document.getElementById("forecast-image-main");
     
-    location.textContent = weatherData.location;
+    location.textContent = capitalizeLocation(weatherData.location);
     temperature.textContent = weatherData.currentTemp;
     conditions.textContent = weatherData.conditions;
     mainImg.src = iconMap[weatherData.icon];
     console.log(weatherData.icon);
+}
+
+function capitalizeLocation(string) {
+    // Splits string by spaces, capitalizes first character, then slices rest of string back in
+    return string.split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
 }
