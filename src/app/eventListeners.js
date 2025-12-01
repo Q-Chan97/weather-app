219523548222fb0229559722488dd1c580f4dbff
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2025-12-01 17:29:02 -0600

    Make runSearch update currentSearch variable

--- src/app/eventListeners.js
+++ src/app/eventListeners.js
@@ -1,9 +1,14 @@
 import { updatePage } from "./weatherUI";
 
+export let currentSearch;
+
 export function runSearch() {
     const searchButton = document.getElementById("search-button");
 
     searchButton.addEventListener("click", () => {
+        const searchTerm = document.getElementById("search-bar").value.trim();
+        currentSearch = searchTerm;
         updatePage();
+        console.log(currentSearch);
     })
 }
