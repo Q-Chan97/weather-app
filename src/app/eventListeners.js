574c4f78f0505d601eba90f48755da85ec4a0243
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2025-12-02 00:27:03 -0600

    Add event listener for enter keydown

--- src/app/eventListeners.js
+++ src/app/eventListeners.js
@@ -5,12 +5,22 @@ export let currentSearch = "Tokyo"; // Default search is Tokyo on page load
 
 export function runSearch() {
     const searchButton = document.getElementById("search-button");
+    const searchInput = document.getElementById("search-bar");
 
     searchButton.addEventListener("click", () => {
         const searchTerm = document.getElementById("search-bar").value.trim();
         currentSearch = searchTerm;
         updatePage();
         console.log(currentSearch);
+    })
+
+    searchInput.addEventListener("keydown", (e) => { // Runs same search if Enter key is pressed instead
+        if (e.key === "Enter") {
+            const searchTerm = document.getElementById("search-bar").value.trim();
+            currentSearch = searchTerm;
+            updatePage();
+            console.log("Search ran with Enter key.");
+        }
     })
 }
 
